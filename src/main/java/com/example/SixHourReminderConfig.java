4dd8077d50f07de6dd740433fbab2bb742ed72c5
Richardant <54707022+Richardant@users.noreply.github.com>
2024-10-04 09:38:50 -0400

    Create SixHourReminderConfig.java

---
+++ src/main/java/com/example/SixHourReminderConfig.java
@@ -0,0 +1,17 @@
+package com.SixHourReminder;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
+import net.runelite.client.config.Units;
+
+@ConfigGroup("sixhourreminder")
+public interface SixHourReminderConfig extends Config {
+    @Units(" mins")
+    @Range(min = 1, max = 60)
+    @ConfigItem(keyName = "sixHourReminderTime", name = "Time until 6hr", description = "Show overlay if time until 6hr log (minutes)", position = 100)
+    default int sixHourReminderTime() {
+        return 30;
+    }
+}
