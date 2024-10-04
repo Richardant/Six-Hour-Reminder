a46e37d2fc2f492951067f4845f419714a8ff854
Richardant <54707022+Richardant@users.noreply.github.com>
2024-10-04 10:42:59 -0400

    Update and rename ExamplePluginTest.java to SixHourReminderTest.java

--- src/test/java/com/example/SixHourReminderTest.java
+++ src/test/java/com/example/SixHourReminderTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.SixHourReminder;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class SixHourReminderTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(SixHourReminderPlugin.class);
 		RuneLite.main(args);
 	}
 }
