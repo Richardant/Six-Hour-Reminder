3afa6a80f3d9088b5ec571eb18bbc46e5845630a
Richardant <54707022+Richardant@users.noreply.github.com>
2024-10-04 10:46:54 -0400

    minor changes

--- src/main/java/com/SixHourReminder/SixHourReminderPanel.java
+++ src/main/java/com/SixHourReminder/SixHourReminderPanel.java
@@ -1,5 +1,11 @@
 package com.SixHourReminder;
 
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.OverlayPriority;
+import net.runelite.client.ui.overlay.components.LineComponent;
+
+import javax.inject.Inject;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -7,11 +13,6 @@ import java.awt.Graphics2D;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
-import javax.inject.Inject;
-import net.runelite.client.ui.overlay.OverlayPanel;
-import net.runelite.client.ui.overlay.OverlayPosition;
-import net.runelite.client.ui.overlay.OverlayPriority;
-import net.runelite.client.ui.overlay.components.LineComponent;
 
 public class SixHourReminderPanel extends OverlayPanel {
     private final SixHourReminderPlugin plugin;
