d97e60a03514dd9ba191c637c7c73a876528ca72
Richardant <54707022+Richardant@users.noreply.github.com>
2024-10-04 14:34:37 -0400

    Final adjustments

--- src/main/java/com/SixHourReminder/SixHourReminderPanel.java
+++ src/main/java/com/SixHourReminder/SixHourReminderPanel.java
@@ -2,13 +2,11 @@ package com.SixHourReminder;
 
 import net.runelite.client.ui.overlay.OverlayPanel;
 import net.runelite.client.ui.overlay.OverlayPosition;
-import net.runelite.client.ui.overlay.OverlayPriority;
 import net.runelite.client.ui.overlay.components.LineComponent;
 
 import javax.inject.Inject;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics2D;
 import java.time.Duration;
 import java.time.Instant;
@@ -18,7 +16,7 @@ public class SixHourReminderPanel extends OverlayPanel {
     private final SixHourReminderPlugin plugin;
 
     private final SixHourReminderConfig config;
-    
+
     @Inject
     private SixHourReminderPanel(SixHourReminderPlugin plugin, SixHourReminderConfig config) {
         setPosition(OverlayPosition.ABOVE_CHATBOX_RIGHT);
